21421147e1dc13741496e599b1f721b36a2d3a63
Aya <aya.almallah75@gmail.com>
2024-05-10 16:48:54 +0300

    start coding for breakable wall

---
+++ Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : MonoBehaviour
+{
+    public GameObject wallBreakParticle;
+    public float maxHealth;
+    private float currentHealth;
+
+    
+    void Start()
+    {
+        currentHealth =  maxHealth;
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0.0f)
+        {
+            Break();
+        }
+    }
+    
+    private void Break()
+    {
+        Instantiate(wallBreakParticle, transform.position, wallBreakParticle.transform.rotation);
+        Destroy(gameObject);
+    }
+}
