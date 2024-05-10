6b4bc7e601b1e0b20ce2b24f595abbf069a02a3c
Abbro078 <portgas.samer@gmail.com>
2024-05-10 16:37:03 +0300

    bug fixes in pasue menus

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public GameObject settingsMenu;
+    public Button backButton;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(settingsMenu.activeSelf)
+            {
+                backButton.onClick.Invoke();
+            }
+        }
+    }
+
     public void setVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
